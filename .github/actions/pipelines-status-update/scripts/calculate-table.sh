90a6f9ef18b9e947ef2fe1a0184c2b43dcd9791c
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2024-05-16 15:47:56 -0400

    fix: Ignore if empty

--- .github/actions/pipelines-status-update/scripts/calculate-table.sh
+++ .github/actions/pipelines-status-update/scripts/calculate-table.sh
@@ -132,7 +132,7 @@ if [[ $NUM_STEPS -gt 0 ]]; then
             echo "$details" >>"$TMPFILE"
         fi
         echo "details extended"
-        echo "$STEP_DETAILS_EXTENDED"
+        echo "${STEP_DETAILS_EXTENDED:-}"
         if [[ -n "${STEP_DETAILS_EXTENDED:-}" ]]; then
             # If the length of `STEP_DETAILS_EXTENDED` is greater than 40000 characters, then we emit a default message instead
             if [[ ${#STEP_DETAILS_EXTENDED} -gt $(getMaximumStepCharacterCount "$NUM_STEPS") ]]; then
